f058cebb850945f8207d8d54aa44c0eea2968b9a
Amund F. P. Larsen <amund.f.p.larsen@gmail.com>
2024-02-01 15:19:19 +0100

    added push

--- src/main/java/Test_push.java
+++ src/main/java/Test_push.java
@@ -2,6 +2,7 @@ public class Test_push {
     public static void main(String[] args) {
         System.out.println("Test push");
         System.out.println("Test push");
+        System.out.println("Test push");
     }
 
 }
