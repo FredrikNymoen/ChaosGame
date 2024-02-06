ecacbe3e3ada78bb5a39def3e7fa46502219410a
Amund F. P. Larsen <amund.f.p.larsen@gmail.com>
2024-02-06 14:39:20 +0100

    Made the Transform2D class and filled it with fitting fields and a Transform2D and multiply function.

---
+++ src/main/java/Transform2D.java
@@ -0,0 +1,19 @@
+public class Transform2D {
+  private double a00;
+  private double a01;
+  private double a10;
+  private double a11;
+
+  public Transform2D(double a00, double a01, double a10, double a11) {
+    this.a00 = a00;
+    this.a01 = a01;
+    this.a10 = a10;
+    this.a11 = a11;
+  }
+
+  public Vector2D multiply(Vector2D vector){
+    double newX = a00 * vector.getX0() + a01 * vector.getX1();
+    double newY = a10 * vector.getX0() + a11 * vector.getX1();
+    return new Vector2D(newX, newY);
+  }
+}
