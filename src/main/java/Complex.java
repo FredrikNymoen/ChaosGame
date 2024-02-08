11d8eaa6c4b2f70bb04cfd7306f46f2532f73b11
Amund F. P. Larsen <amund.f.p.larsen@gmail.com>
2024-02-08 15:46:13 +0100

    removed real and imaginary fields and used x0 and x1 instead

--- src/main/java/Complex.java
+++ src/main/java/Complex.java
@@ -1,22 +1,12 @@
 public class Complex extends Vector2D{
-  private double realPart;
-  private double imaginaryPart;
 
-  public Complex(double realPart, double imaginaryPart) {
-    this.realPart = realPart;
-    this.imaginaryPart = imaginaryPart;
+  public Complex(double realpart, double imaginarypart) {
+    super(realpart, imaginarypart);
   }
 
   public Complex sqrt() {
-    double r = Math.sqrt(realPart);
-    double i = Math.sqrt(imaginaryPart);
+    double r = Math.sqrt(getX0());
+    double i = Math.sqrt(getX1());
     return new Complex(r, i);
-  }
-
-  public double getRealPart() {
-    return realPart;
-  }
-  public double getImaginaryPart() {
-    return imaginaryPart;
   }
 }
