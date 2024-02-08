485cf5dd6bfc80b0ad4b2b024094a597d330e946
fredriknymoen <fenymoen@stud.ntnu.no>
2024-02-08 15:23:54 +0100

    Changed transform method in JuliaTransform

--- src/main/java/JuliaTransform.java
+++ src/main/java/JuliaTransform.java
@@ -11,10 +11,10 @@ public class JuliaTransform extends Transform2D{
 
   @Override
   public Vector2D transform(Vector2D point) {
-    // Konverterer Vector2D til et komplekst tall z
+    // Konverterer this.point til et komplekst tall c
     Vector2D c = new Vector2D(this.point.getRealPart(), this.point.getImaginaryPart());
     // Beregner z - c
-    Vector2D zMinusC = point.subtract(this.point);
+    Vector2D zMinusC = point.subtract(c);
     // Beregner kvadratroten av det komplekse tallet, avhengig av sign
     Complex z = new Complex(zMinusC.getX0(), zMinusC.getX1()).sqrt();
     if (this.sign < 0) {
