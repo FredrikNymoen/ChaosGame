8a4eff8ad4cd6e35111b1f2fbe1a7de40e02406d
fredriknymoen <fenymoen@stud.ntnu.no>
2024-02-22 15:21:06 +0100

    worked on ChaosCanvas

--- src/main/java/ChaosCanvas.java
+++ src/main/java/ChaosCanvas.java
@@ -1,3 +1,4 @@
+import mathcore.Matrix2x2;
 import mathcore.Vector2D;
 import transformations.AffineTransform2D;
 
@@ -14,17 +15,24 @@ public class ChaosCanvas {
     this.height = height;
     this.minCoords = minCoords;
     this.maxCoords = maxCoords;
+    canvas = new int[width][height];
   }
   public int getPixel(Vector2D point) {
     return canvas[(int) point.getX0()][(int) point.getX1()];
   }
+
   public void putPixel(Vector2D point){
-    canvas[(int) point.getX0()][(int) point.getX1()] = 1;
+    Matrix2x2 matrix = new Matrix2x2(0, (height - 1) / (minCoords.getX1() - maxCoords.getX1()), (width - 1) / (maxCoords.getX0() - minCoords.getX0()), 0);
+    Vector2D vector = new Vector2D(((height - 1) * maxCoords.getX1()) / (maxCoords.getX1() - minCoords.getX1()), ((width - 1) * minCoords.getX0()) / (minCoords.getX0() - maxCoords.getX0()));
+    transformCoordsToIndices = new AffineTransform2D(matrix, vector);
+    Vector2D indices = transformCoordsToIndices.transform(point);
+    canvas[(int) indices.getX0()][(int) indices.getX1()] = 1;
   }
+
   public int[][] getCanvasArray(){
     return canvas;
   }
   public void clear(){
-    //canvas = new int[width][height];
+    canvas = new int[width][height];
   }
 }
