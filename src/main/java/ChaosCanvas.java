f5e6df2d25fd90c8b47b6c7156428e9a2c915a1b
fredriknymoen <fenymoen@stud.ntnu.no>
2024-02-22 15:38:49 +0100

    jobbed more on the ChaosCanvas

--- src/main/java/ChaosCanvas.java
+++ src/main/java/ChaosCanvas.java
@@ -18,7 +18,11 @@ public class ChaosCanvas {
     canvas = new int[width][height];
   }
   public int getPixel(Vector2D point) {
-    return canvas[(int) point.getX0()][(int) point.getX1()];
+    Matrix2x2 matrix = new Matrix2x2(0, (height - 1) / (minCoords.getX1() - maxCoords.getX1()), (width - 1) / (maxCoords.getX0() - minCoords.getX0()), 0);
+    Vector2D vector = new Vector2D(((height - 1) * maxCoords.getX1()) / (maxCoords.getX1() - minCoords.getX1()), ((width - 1) * minCoords.getX0()) / (minCoords.getX0() - maxCoords.getX0()));
+    transformCoordsToIndices = new AffineTransform2D(matrix, vector);
+    Vector2D indices = transformCoordsToIndices.transform(point);
+    return canvas[(int) indices.getX0()][(int) indices.getX1()];
   }
 
   public void putPixel(Vector2D point){
@@ -33,6 +37,10 @@ public class ChaosCanvas {
     return canvas;
   }
   public void clear(){
-    canvas = new int[width][height];
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        canvas[i][j] = 0;
+      }
+    }
   }
 }
--- src/main/java/ChaosGame.java
+++ src/main/java/ChaosGame.java
@@ -1,2 +1,29 @@
+import java.util.Random;
+import mathcore.Vector2D;
+import transformations.Transform2D;
+
 public class ChaosGame {
+  private ChaosCanvas canvas;
+  private ChaosGameDescription description;
+  private Vector2D currentPoint;
+  public Random random;
+
+  public ChaosGame(ChaosGameDescription description, int width, int height) {
+    this.description = description;
+    this.canvas = new ChaosCanvas(width, height, description.getMinCoords(), description.getMaxCoords());
+    this.currentPoint = new Vector2D(0, 0);
+    this.random = new Random();
+  }
+  public ChaosCanvas getCanvas() {
+    return canvas;
+  }
+
+  public void runSteps(int steps) {
+    for (int i = 0; i < steps; i++) {
+      Transform2D transform = description.getTransforms().get(random.nextInt(description.getTransforms().size()));
+      currentPoint = transform.transform(currentPoint);
+      canvas.putPixel(currentPoint);
+    }
+  }
+
 }
