92e58372723c33cf6e726ad3eeb6af56d6ca3d34
Amund F. P. Larsen <amund.f.p.larsen@gmail.com>
2024-02-27 14:06:24 +0100

    chaosGameFileHandler updated

--- src/main/java/ChaosGameFileHandler.java
+++ src/main/java/ChaosGameFileHandler.java
@@ -1,3 +1,20 @@
-public class ChaosGameFileHandler{
+import java.io.BufferedWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import transformations.Transform2D;
 
+public class ChaosGameFileHandler{
+  ChaosGameDescription readFromFile(String path){
+    return null;
+  }
+  ChaosGameDescription writeToFile(ChaosGameDescription description, String path){
+    try(BufferedWriter writer = Files.newBufferedWriter(Path.of(filename))){
+      writer.write(description.getMinCoords().getX0() + " " + description.getMinCoords().getX1() + "\n");
+      writer.write(description.getMaxCoords().getX0() + " " + description.getMaxCoords().getX1() + "\n");
+    }
+    catch (Exception e){
+      e.printStackTrace();
+    }
+    return description;
+  }
 }
