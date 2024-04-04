daf0d3d0ff1219630f95546082c4340752467528
Amund F. P. Larsen <amund.f.p.larsen@gmail.com>
2024-04-04 13:30:19 +0200

    changed back to file.csv

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -210,8 +210,8 @@ public class Main {
     boolean gameRunning = true;
     while (gameRunning) {
       ChaosGameDescription description = gameQuestions();
-      fileHandler.writeToFile(description, getClass().getResource("file.csv").toString());
-      description = fileHandler.readFromFile(getClass().getResource("file.csv").toString());
+      fileHandler.writeToFile(description, "file.csv");
+      description = fileHandler.readFromFile("file.csv");
       ChaosGame game = new ChaosGame(description, 250, 100);
       int iterations = askForIterations();
       game.runSteps(iterations);
@@ -235,6 +235,4 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     return scanner.nextInt();
   }
-
-
 }
